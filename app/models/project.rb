042c590296644de93bb7993ebdd5f42fcbe56f12
Piotr Paściak <piotr.pasciak@besidethepark.com>
2016-10-24 12:45:45 +0200

    Delete of sensitive information files

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,10 @@
 class Project < ActiveRecord::Base
-  has_many :images
+  has_many :images, dependent: :destroy
+
+  validates :content, presence: true, length: { minimum: 5 }
+  validates :title, presence: true, length: { minimum: 5 }
+
+  validates :location, presence: true
+  validates :owner, presence: true
+  validates :company, presence: true
 end
